e3935b80435b4c36a5fa0790bbe4316512e9b962
Avishai Lazar <avishai1984@gmail.com>
2021-12-20 08:36:05 +0200

    more stuff

--- extension/content.js
+++ extension/content.js
@@ -124,7 +124,10 @@ selectMacro = function(targetId, macro) {
 
     setTimeout(() => {
         updateTopUsages(macro);
-        $.post( "https://us-central1-github-macros.cloudfunctions.net/mutate/", { type: "use", name: macro["name"] } );
+        $.post( 
+            "https://us-central1-github-macros.cloudfunctions.net/mutate/",
+            { type: "use", name: macro["name"] },
+        );
     }, 0);
 }
 
@@ -189,13 +192,7 @@ updateCacheWithNewContent = function(searchText, items, isRealPage, hasMore) {
     if (isRealPage) {
         cachedContent['next_page'] = cachedContent['next_page'] + 1;
     }
-    cachedContent['has_more'] = hasMore;    
-
-    if (isNewPage) {
-        
-    } else {
-        cachedContent['has_more'] = true;
-    }
+    cachedContent['has_more'] = hasMore;
 }
 
 updateUIWithContent = function (targetId, content, isFirstPage) {
@@ -223,7 +220,7 @@ updateUIWithContent = function (targetId, content, isFirstPage) {
         image.title = item['name'];
         image.onclick = function() {selectMacro(targetId, item);};
         image.onerror = function () {
-            $.post( "https://us-central1-github-macros.cloudfunctions.net/mutate/", { type: "report", name: macro["name"] } );
+            $.post( "https://us-central1-github-macros.cloudfunctions.net/mutate/", { type: "report", name: item["name"] } );
         };
         divs[i % 2].appendChild(image);
     }
@@ -233,7 +230,9 @@ createTooltip = function(targetId, target) {
     const idPrefix = targetId + '_';
     const magGlassSrc = chrome.runtime.getURL('img/icons/mglass-4x.png');
     const plusSrc = chrome.runtime.getURL('img/icons/plus-4x.png');
-
+    const errorXSrc = chrome.runtime.getURL('img/icons/x-4x.png');
+    const successVSrc = chrome.runtime.getURL('img/icons/v-4x.png');
+    
     var fetchWasCalled = false;
 
     return new jBox(
@@ -296,7 +295,7 @@ createTooltip = function(targetId, target) {
                     <div style="display: flex; flex-direction: row; width: 100%; height: auto; margin: 10px; border-width: 1px; border-style: solid none solid solid; border-color: #234C87; border-radius: 18px; overflow: auto">
                         <div style="display: flex; margin-left: 10px; width: 15px; height: 15px; justify-content: center; align-items: center; height: 100%; ">
                             <img id="${idPrefix}searchIcon" style="width: 15px; height: 15px;" src="${magGlassSrc}" />
-                            <div id="${idPrefix}searchSpinner" style="display: none;" class="gh-macros-loader"></div>
+                            <div id="${idPrefix}searchSpinner" style="display: none;" class="gh-macros-loader gh-macros-dark-loader"></div>
                         </div>
                         <div style="display: flex; justify-content: center; align-items: center; height: 100%; flex-grow: 1;">
                             <input class="gh-macros-no-outline" type="text" id="${idPrefix}macroSearchInput" style="width: 100%; padding-left: 12px;">
@@ -314,12 +313,50 @@ createTooltip = function(targetId, target) {
                         </div>
                     </div>
                     <div id="${idPrefix}moreResultsSpinner" style="width: 100%; height: 40px;flex-shrink: 0; align-items: center; display: flex;">
-                        <div class="gh-macros-loader" style="font-size: 2px;"></div>
+                        <div class="gh-macros-loader gh-macros-dark-loader" style="font-size: 2px;"></div>
                     </div>
                 </div>
-                <div id="${idPrefix}addNewMacro" class="gh-macros-box-shadow gh-macros-hoover-bg" style="z-index: 100; display: flex; justify-content: center; align-items: center; position: absolute; right: 16px; bottom: 16px; width: 30px; height: 30px; background-color: #234C87; border-radius: 15px;">
+                <div id="${idPrefix}openMacroCreationButton" class="gh-macros-box-shadow gh-macros-hoover-bg" style="z-index: 1; display: flex; justify-content: center; align-items: center; position: absolute; right: 16px; bottom: 16px; width: 30px; height: 30px; background-color: #234C87; border-radius: 15px;">
                     <img style="width: 7px; height: 7px" src="${plusSrc}" />
                 </div>
+                <div id="${idPrefix}addNewMacroWrapper" style="z-index: 2; display: flex; flex-direction: column; position: absolute; bottom: 0; left:0; right: 0; width: 100%; height: 0; align-items: end">
+                    <div id="${idPrefix}addNewMacroCloseMargins" style="height: 150px; width: 100%"></div>
+                    <div style="display: flex; flex-direction: column; height: 100%; width: 100%; background-color: #234C87; border-radius: 10px 10px 0px 0px;">
+                        <div style="text-align: right; margin:10px 10px 0 10px;">
+                            <text id="${idPrefix}addNewMacroCloseButton" style="font-family: 'Pragati Narrow'; font-weight: 700; font-size: 12px; color: white; user-select: none;">X</text>
+                        </div>
+
+                        <div id="${idPrefix}addNewMacroEdit" style="display: flex; flex-direction: column; width: 100%; height: 100%">
+                            <div style="margin: 0 15px 15px 15px">
+                                <text style="font-family: 'Pragati Narrow'; font-weight: 400; font-size: 12px; color: white; user-select: none;">Name</text>
+                                <div style="border-radius: 5px; background-color: #ffffff; overflow: hidden;">
+                                    <input class="gh-macros-no-outline" type="text" id="${idPrefix}newMacroName" style="width: 100%; margin: 4px;">
+                                </div>    
+                            </div>
+                            <div style="margin: 0 15px 15px 15px">
+                                <text style="font-family: 'Pragati Narrow'; font-weight: 400; font-size: 12px; color: white; user-select: none;">URL</text>
+                                <div style="border-radius: 5px; background-color: #ffffff; overflow: hidden;">
+                                    <input class="gh-macros-no-outline" type="text" id="${idPrefix}newMacroURL" style="width: 100%; margin: 4px;">
+                                </div>    
+                            </div>
+                            <div id="${idPrefix}addNewMacroButton" style="display: flex; border-radius: 4px; margin: 15px 15px 15px 15px; background-color: #FFFFFF; justify-content: center;">
+                                <text style="font-family: 'Pragati Narrow'; font-weight: 700; font-size: 12px; color: black; user-select: none; margin: 4px;">Add to the pile</text>
+                            </div>
+                            <div style="display: flex; flex-grow: 1; flex-direction: column; margin: 0px 15px 15px 15px; justify-content: center;">
+                                <div id="${idPrefix}addNewMacroSpinner" class="gh-macros-light-loader gh-macros-loader" style="display: none; font-size: 2px;"></div>
+                                <div id="${idPrefix}addNewMacroError" style="display: none; flex-direction: column; align-items: center; text-align: center;">
+                                    <img src="${errorXSrc}" style="width: 16px; height: 16px"/>
+                                    <div id="${idPrefix}addNewMacroErrorMessage" class="gh-macros-add-new-error-message">
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                        <div id="${idPrefix}addNewMacroSuccess" style="display: none;flex-direction: column;width: 100%;height: 100%;align-items: center;justify-content: center;">
+                            <img src="${successVSrc}" style="width: 34px;height: 25px;">
+                            <text style="font-family: 'Pragati Narrow';font-weight: 400;font-size: 16px;color: white;user-select: none;margin: 20;text-align: center;">It was added succesfully to the pile and you can start using it! Thanks for contributing :)</text>
+                        </div>   
+                    </div>  
+                </div> 
             </div>
             `,
         },
@@ -354,6 +391,7 @@ handleCloseTooltip = function(targetId) {
     getElement(targetId, 'leftMacros').innerHTML = '';
     getElement(targetId, 'rightMacros').innerHTML = '';
     getElement(targetId, 'moreResultsSpinner').style.display = 'inline';
+    hideAddMacroUI(targetId);
     targetIdToTarget.get(targetId).focus();
 }
 
@@ -382,11 +420,173 @@ initMacrosScrollLogic = function(targetId) {
       });
 }
 
+showAddMacroUI = function(targetId) {
+    addNewMacroUI = getElement(targetId, 'addNewMacroWrapper')
+    $(addNewMacroUI).animate({'height': '100%'});
+}
+
+hideAddMacroUI = function(targetId) {
+    addNewMacroUI = getElement(targetId, 'addNewMacroWrapper')
+    $(addNewMacroUI).animate({'height': '0%'});
+
+    getElement(targetId, 'addNewMacroSuccess').style.display = 'none';
+    getElement(targetId, 'addNewMacroEdit').style.display = 'flex';
+    getElement(targetId, 'newMacroName').value = '';
+    getElement(targetId, 'newMacroURL').value = '';
+    getElement(targetId, 'addNewMacroSpinner').style.display = 'none';
+    getElement(targetId, 'addNewMacroError').style.display = 'none';
+    getElement(targetId, 'addNewMacroErrorMessage').innerHTML = '';
+}
+
 initAddNewMacroLogic = function(targetId) {
-    addNewMacro = getElement(targetId, 'addNewMacro');
-    addNewMacro.onmouseover = function() {addNewMacro.style.background='#526683'};
-    addNewMacro.onmouseout = function() {addNewMacro.style.background='#234C87'};
-    addNewMacro.onclick = function() {alert('click')};
+    // set up add new macro button
+    openMacroCreationButton = getElement(targetId, 'openMacroCreationButton');
+    openMacroCreationButton.onmouseover = function() {openMacroCreationButton.style.background='#526683'};
+    openMacroCreationButton.onmouseout = function() {openMacroCreationButton.style.background='#234C87'};
+    openMacroCreationButton.onclick = function() {showAddMacroUI(targetId)};
+
+    // set up add new macro UI
+    getElement(targetId, 'addNewMacroCloseMargins').onclick = () => {hideAddMacroUI(targetId);}
+    getElement(targetId, 'addNewMacroCloseButton').onclick = () => {hideAddMacroUI(targetId);}
+
+    getElement(targetId, 'addNewMacroButton').onclick = () => {addNewMacro(targetId);}
+    
+}
+
+addNewMacroShowErrorMessage = function(targetId, errorHTML) {
+    getElement(targetId, 'addNewMacroSpinner').style.display = 'none';
+    getElement(targetId, 'addNewMacroError').style.display = 'flex';
+    getElement(targetId, 'addNewMacroErrorMessage').innerHTML = errorHTML;
+}
+
+addNewMacroShowSuccessMessage = function(targetId) {
+    getElement(targetId, 'addNewMacroEdit').style.display = 'none';
+    getElement(targetId, 'addNewMacroSuccess').style.display = 'flex';
+}
+
+validateInput = function(macroName, macroURL) {
+    if (macroName === '') {
+        return {
+            'is_valid': false,
+            'error': 'Name is empty',
+        }
+    }
+
+    if (macroName.includes(' ')) {
+        return {
+            'is_valid': false,
+            'error': 'Name can\'t contain spaces',
+        }  
+    }
+
+    if (macroURL === '') {
+        return {
+            'is_valid': false,
+            'error': 'URL is empty',
+        } 
+    }
+
+    try {
+        new URL(macroURL);
+    } catch {
+        return {
+            'is_valid': false,
+            'error': 'URL is not valid',
+        } 
+    }
+
+    return {
+        'is_valid': true,
+    }
+}
+
+const addingNewMacro = false
+
+addNewMacro = function(targetId) {
+    if (addingNewMacro) {
+        return
+    }
+
+    // $.ajax({
+    //     url: 'https://api.resmush.it/ws.php?img=https://user-images.githubusercontent.com/10358078/142379224-23b6e6e5-d45d-4bc6-a183-733b831a622d.jpeg',
+    //     success: function(responseText) {
+    //         console.log(responseText)
+    //     },
+    // });
+    getElement(targetId, 'addNewMacroError').style.display = 'none';
+    getElement(targetId, 'addNewMacroSpinner').style.display = 'inline';
+
+    const macroName = getElement(targetId, 'newMacroName').value;
+    const macroURL = getElement(targetId, 'newMacroURL').value;
+
+    is_valid_res = validateInput(macroName, macroURL)
+
+    if (!is_valid_res['is_valid']) {
+        addNewMacroShowErrorMessage(targetId, is_valid_res['error']);
+        return
+    }
+
+    addingNewMacro = true;
+
+    const img = document.createElement('img')
+    img.onload = () => {
+        $.post(
+            "https://us-central1-github-macros.cloudfunctions.net/mutate/",
+            { type: "add", name: macroName, url: macroURL },
+            (responseText) => {
+                const response = JSON.parse(responseText);
+                if (!(response['success'])) {
+                    addNewMacroShowErrorMessage(targetId, response["error_message"] || "Unable to process the request. Check the URL and try again"); 
+                    return
+                }
+
+                addNewMacroShowSuccessMessage();
+
+                macroNameToUrl[macroName] = macroURL
+                contentCache.forEach(
+                    (key, value) => {
+                        if (macroName.includes(key)) {
+                            value['data'].unshift({'name': macroName, 'url': macroURL})
+                        }
+                    },
+                )
+            },
+        ).fail(
+            () => {
+                addNewMacroShowErrorMessage(targetId, "Unable to process the request. Check the URL and try again"); 
+            },
+        ).always(() => {addingNewMacro = false;});
+        
+    }
+    img.onerror = () => {
+        addNewMacroShowErrorMessage(targetId, "Unable to load the image. Please drop the image into the comment box and use the URL");
+        addingNewMacro = false;
+    }
+    img.src = macroURL;
+
+    /*
+     * 1. inject the URL
+     * 2. trigger preview by sending this event to the Preview button:
+     *  we can query all buttons with Preview text in it and send this event to the button
+     *  const mouseoverEvent = new Event('mouseenter');
+     *  previewButton.dispatchEvent(mouseoverEvent);
+     * 3. search the dom for the new image
+     * 4. extract the URL
+     * 5. remove the injected URL + restore mouse cursor
+     * 6. In case of a failure, let the user know they will have to do it manually
+     */
+
+    /*
+     * proposal for supporting thumbnails
+     * after adding the initial image, load it in JS and, reduce its size and then
+     * use the same tecnique to create another github image. Same for Gifs where we want
+     * to have an image instead with icon of "gif" on top of it.
+     * https://stackoverflow.com/questions/42092640/javascript-how-to-reduce-image-to-specific-file-size
+     * 
+     * reduce size of images: https://jsfiddle.net/qnhmytk4/3/
+     * gid image extraction: https://jsfiddle.net/7gtLtkbw/
+     */
+    
 }
 
 initNewTooltip = function(targetId) {
@@ -587,8 +787,5 @@ window.onload = function() {
 
     initKeyboardListeners();
     loadSuggestionsFromStorage()
-    // chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
-    //     console.log('Settings saved');
-    // });
     processGithubMacroImages();
 }
